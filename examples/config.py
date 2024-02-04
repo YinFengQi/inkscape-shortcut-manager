9b45cb5c51be41657d20921e87e29603d9512c0a
mny <mny23@mails.tsinghua.edu.cn>
2024-02-04 21:53:13 +0800

    修改了gnome terminal窗口的位置

--- examples/config.py
+++ examples/config.py
@@ -6,10 +6,8 @@ def open_editor(filename):
 
     subprocess.run([
         'gnome-terminal','--wait',
-        # '-fn', 'xft:JetBrains mono nerd font:pixelsize=24',
-        '--geometry', '60x8',
+        '--geometry', '60x6+600+800',
         '--title', 'latex input box',
-        # '-background','gray40', '-cursorColor','gray80',
         '--', "nvim",
         "-u", "~/.config/nvim/minimal-tex-nvim.lua",
         f"{filename}",
@@ -18,7 +16,7 @@ def open_editor(filename):
 
 def latex_document(latex):
     return r"""
-        \documentclass{standalone}
+        \documentclass[12pt,border=12pt]{standalone}
         \usepackage{amsfonts,amssymb,amsthm,amsmath,physics,extarrows}
         \begin{document}
     """ + latex + r"\end{document}"
